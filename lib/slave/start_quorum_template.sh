31fbde92b8d0a51e311bd6708149891a30b6c174
Matus Kysel <matuskysel1@gmail.com>
2019-07-12 13:54:31 +0200

    gethlog

--- lib/slave/start_quorum_template.sh
+++ lib/slave/start_quorum_template.sh
@@ -45,7 +45,8 @@ upcheck
 echo "[*] Starting ${NODENAME} node" >> qdata/gethLogs/${NODENAME}.log
 echo "[*] geth --verbosity 6 --datadir qdata --permissioned --nodiscover --istanbul.blockperiod 5 --syncmode full --mine --minerthreads 1 --networkid $NETID --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul --emitcheckpoints --rpcport $R_PORT --port $W_PORT --nat extip:$CURRENT_NODE_IP">> qdata/gethLogs/${NODENAME}.log
 
-PRIVATE_CONFIG=qdata/$NODENAME.ipc geth --verbosity 6 --datadir qdata $GLOBAL_ARGS --rpccorsdomain "*" --port $W_PORT --ws --wsaddr 0.0.0.0 --wsport $WS_PORT --wsorigins '*' --wsapi $ENABLED_API --nat extip:$CURRENT_NODE_IP 
+PRIVATE_CONFIG=qdata/$NODENAME.ipc geth --verbosity 6 --datadir qdata $GLOBAL_ARGS --rpccorsdomain "*" --port $W_PORT --ws --wsaddr 0.0.0.0 --wsport $WS_PORT --wsorigins '*' --wsapi $ENABLED_API --nat extip:$CURRENT_NODE_IP 2>>qdata/gethLogs/${NODENAME}.log &
+
 
 cd /root/quorum-maker/
 ./start_nodemanager.sh -r $R_PORT -g $NM_PORT -c $CHAIN_ID -m $MINING_FLAG -p $PK
